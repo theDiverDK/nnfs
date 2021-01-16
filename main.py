fa1df20959f91d56dd0769bfea12cfa58a102916
Søren Reinke <sre@ooono.dk>
2021-01-16 14:20:06 +0100

    Transpose

--- main.py
+++ main.py
@@ -9,5 +9,22 @@ weights = [[0.2, 0.8, -0.5, 1.0],
 bias = [2.0, 3.0, 0.5]
 
 output = np.dot(weights, inputs) + bias
-
 print(output)
+
+print('---')
+
+a = [1, 2, 3]
+print(np.array(a))
+print(np.array([a]))
+print(np.expand_dims(np.array(a), axis=0))
+
+print('---')
+
+b = [2, 3, 4]
+
+a = np.array([a])
+b = np.array([b]).T
+
+print(a, b)
+print(np.dot(a, b))
+print('---')
