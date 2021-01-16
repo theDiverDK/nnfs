e14761fb840818b911c617d289e56320735a3507
Søren Reinke <sre@ooono.dk>
2021-01-16 13:16:28 +0100

    3 neurons

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
-# This is a sample Python script.
+inputs = [1, 2, 3]
+weights = [0.2, 0.8, -0.5]
+bias = 2
 
-# Press ⇧F10 to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+output = (inputs[0] * weights[0] +
+          inputs[1] * weights[1] +
+          inputs[2] * weights[2] + bias)
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print(output)
