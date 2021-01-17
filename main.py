3ffef2754a04d2309d552ea75cfcc9598ce317c0
Søren Reinke <sre@ooono.dk>
2021-01-17 12:07:46 +0100

    Loss_CategoricalCrossentrophy

--- main.py
+++ main.py
@@ -11,6 +11,42 @@ class Activation_ReLU:
         self.output = np.maximum(0, inputs)
 
 
+class Activation_Softmax:
+    def forward(self, inputs):
+        exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+        probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+
+        self.output = probabilities
+
+
+class Loss:
+    def calculate(self, output, y):
+        sample_losses = self.forward(output, y)
+        data_loss = np.mean(sample_losses)
+
+        return data_loss
+
+
+class Loss_CategoricalCrossentrophy(Loss):
+    def forward(self, y_pred, y_true):
+        samples = len(y_pred)
+        y_pred_clipped = np.clip(y_pred, 1e-7, 1 - 1e-7)
+
+        if len(y_true.shape) == 1:
+            correct_confidences = y_pred_clipped[
+                range(samples),
+                y_true
+            ]
+        elif len(y_true.shape) == 2:
+            correct_confidences = np.sum(
+                y_pred_clipped * y_true,
+                axis=1
+            )
+
+        negative_log_likelihoods = -np.log(correct_confidences)
+        return negative_log_likelihoods
+
+
 class Layer_Dense:
     def __init__(self, n_inputs, n_neurons):
         self.weights = 0.01 * np.random.randn(n_inputs, n_neurons)
@@ -23,7 +59,19 @@ class Layer_Dense:
 X, y = spiral_data(samples=100, classes=3)
 dense1 = Layer_Dense(2, 3)
 activation1 = Activation_ReLU()
+
+dense2 = Layer_Dense(3, 3)
+activation2 = Activation_Softmax()
+
+loss_function = Loss_CategoricalCrossentrophy()
+
 dense1.forward(X)
 activation1.forward(dense1.output)
-print(dense1.output[:5])
-print(activation1.output[:5])
+
+dense2.forward(activation1.output)
+activation2.forward(dense2.output)
+
+print(activation2.output[:5])
+
+loss = loss_function.calculate(activation2.output, y)
+print('loss:', loss)
